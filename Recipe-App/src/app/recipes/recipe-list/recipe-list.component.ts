4351e1011870acfd161d8528cf0f0d15b280409b
Keith <keithhuels@yahoo.com>
2021-09-22 10:40:03 -0500

    created templates for app

---
+++ Recipe-App/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { RecipeItemComponent } from '../recipe-item/recipe-item.component';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2020%2F02%2F2276935.jpg&q=85"),
+    new Recipe("A Test Recipe", "This is simply a test", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2020%2F02%2F2276935.jpg&q=85")
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Recipe-App/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Onions', 10),
+    new Ingredient('Apples', 5)
+  ]
+  
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+}
